9ccba32537d98a12a9ee9aa8d58ba78ee0b0c789
Ashton Clemens <clemens.genius@gmail.com>
2024-11-08 11:39:36 -0600

    Changed drive data collection from WMI to CIM. PowerShell 7 removed support for WMI.

--- powershell/get-drivespace.ps1
+++ powershell/get-drivespace.ps1
@@ -1,14 +1,20 @@
 ﻿# Function to get disk usage and output it with color
 function Get-DiskUsage {
     # Get the logical disk information
-    $drives = Get-WmiObject -Class Win32_LogicalDisk -Filter "DriveType = 3"
+    $drives = Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DriveType = 3"
 
     foreach ($drive in $drives) {
         $driveLetter = $drive.DeviceID
         $totalSpace = [math]::round($drive.Size / 1GB, 2)
         $freeSpace = [math]::round($drive.FreeSpace / 1GB, 2)
         $usedSpace = $totalSpace - $freeSpace
-        $percentFree = [math]::round(($freeSpace / $totalSpace) * 100, 2)
+
+        # Check if total space is zero to avoid division by zero
+        if ($totalSpace -ne 0) {
+            $percentFree = [math]::round(($freeSpace / $totalSpace) * 100, 2)
+        } else {
+            $percentFree = 0
+        }
 
         # Determine color based on usage
         if ($percentFree -ge 20) {
@@ -26,4 +32,3 @@ function Get-DiskUsage {
 
 # Start running the disk usage report immediately when the script runs
 Get-DiskUsage
-
